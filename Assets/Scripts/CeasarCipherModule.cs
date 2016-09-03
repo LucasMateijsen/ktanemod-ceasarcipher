4892f5fbe84e7ca6ac6dd7950772e1e8dee3b3a3
Lucas Mateijsen <Lucas Mateijsen>
2016-09-03 15:19:52 +0200

    initial commit

---
+++ Assets/Scripts/CeasarCipherModule.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class CeasarCipherModule : CipherModule
+{
+	public TextMesh DisplayText;
+
+	void Start()
+	{
+		DisplayText.text = string.Empty;
+        Init();
+		GetComponent<KMBombModule>().OnActivate += SetDisplay;
+	}
+
+	protected override void Solve()
+	{
+        if (Answer.Length != 5) return;
+
+		if (Ccp.CheckAnswer(Answer.ToCharArray()))
+			GetComponent<KMBombModule>().HandlePass();
+		else
+			GetComponent<KMBombModule>().HandleStrike();
+
+		Answer = string.Empty;
+	}
+
+	private void SetDisplay()
+    {
+        int batteryCount = 0;
+        List<string> responses = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_BATTERIES, null);
+        foreach (string response in responses)
+        {
+            Dictionary<string, int> responseDict = JsonConvert.DeserializeObject<Dictionary<string, int>>(response);
+            batteryCount += responseDict["numbatteries"];
+        }
+
+        Ccp.CalculateOffset(batteryCount);
+        DisplayText.text = Ccp.GetDisplayText();
+	}
+}
---
+++ Assets/Scripts/CeasarCipherPuzzle.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class CeasarCipherPuzzle
+{
+    public int[] PuzzleWord;
+    public int Offset;
+    
+    public CeasarCipherPuzzle()
+    {
+        // Generate word with 5 random characters
+        var randomChars = new List<int>();
+        while (randomChars.Count < 5)
+        {
+            var j = UnityEngine.Random.Range(65, 91);
+            if (randomChars.Contains(j) == false)
+            {
+                randomChars.Add(j);
+            }
+        }
+        PuzzleWord = randomChars.ToArray();
+    }
+
+    public void CalculateOffset(int batteryCount)
+    {
+        Offset = batteryCount;
+    }
+
+    public bool CheckAnswer(char[] answer)
+    {
+        var charsAsInt = answer.Select(x => (int) x).ToArray();
+
+        for (var i = 0; i < PuzzleWord.Length; i++)
+        {
+            if (charsAsInt[i] != PuzzleWord[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public string[] GetButtonText()
+    {
+        var uniqueChars = new List<int>();
+        uniqueChars.AddRange(PuzzleWord);
+
+        while (uniqueChars.Count < 12)
+        {
+            var charInt = UnityEngine.Random.Range(65, 91);
+            if (uniqueChars.Contains(charInt) == false)
+            {
+                uniqueChars.Add(charInt);
+            }
+        }
+
+        var chars = uniqueChars.Select(x => (char)x);
+
+        return chars.Select(x => x.ToString()).ToArray();
+    }
+
+    public string GetDisplayText()
+    {
+        var text = "";
+
+        for(int i = 0; i < PuzzleWord.Length; i++)
+        {
+            text += (char) GetCharWithOffset(PuzzleWord[i], Offset);
+        }
+
+        return text;
+    }
+
+    private int GetCharWithOffset(int ch, int offset)
+    {
+        int result = ch + offset;
+
+        if (result > 90)
+        {
+            result = 64 + (result - 90);
+        }
+
+        return result;
+    }
+}
