7cdc499f615695a6288a704caf64ea4258d4f3aa
Timwi <github@timwi.de>
2016-11-03 10:47:50 +0100

    Overhaul the source for readability. In the process, fix a bug where '[' would come up in the ciphertext.

--- Assets/Scripts/CeasarCipherModule.cs
+++ Assets/Scripts/CeasarCipherModule.cs
@@ -1,68 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CaesarCipher;
 using UnityEngine;
 
-public class CeasarCipherModule : CipherModule
+using Rnd = UnityEngine.Random;
+
+public class CeasarCipherModule : MonoBehaviour
 {
+    // ReSharper disable once InconsistentNaming
+    public KMAudio Audio;
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMSelectable[] Buttons;
+    public TextMesh[] ButtonLabels;
     public TextMesh DisplayText;
+
+    private string _puzzleWord;
+    private string _answer = "";
 
     void Start()
     {
-        DisplayText.text = string.Empty;
-        Init();
-        GetComponent<KMBombModule>().OnActivate += SetDisplay;
+        DisplayText.text = "";
+        Module.OnActivate += Activate;
+
+        // Generate 12 random characters to display on the buttons
+        var randomChars = new List<char>();
+        while (randomChars.Count < 12)
+        {
+            var ch = (char) Rnd.Range(65, 91);
+            if (!randomChars.Contains(ch))
+                randomChars.Add(ch);
+        }
+
+        // The solution consists of 5 of those characters
+        _puzzleWord = new string(randomChars.Take(5).ToArray());
+        Debug.Log("[CaesarCipher] Solution is " + _puzzleWord);
+
+        // Re-scramble the order and then label the buttons
+        var i = 0;
+        while (randomChars.Count > 0)
+        {
+            var index = Rnd.Range(0, randomChars.Count);
+            var letter = randomChars[index];
+            randomChars.RemoveAt(index);
+
+            ButtonLabels[i].text = letter.ToString();
+            Buttons[i].OnInteract += delegate
+            {
+                Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
+                _answer += letter;
+                if (_answer.Length == _puzzleWord.Length)
+                {
+                    if (_answer == _puzzleWord)
+                        Module.HandlePass();
+                    else
+                        Module.HandleStrike();
+                    _answer = "";
+                }
+                return false;
+            };
+            i++;
+        }
     }
 
-    protected override void Solve()
+    private void Activate()
     {
-        if (Answer.Length != 5) return;
+        var serial = Bomb.GetSerialNumber();
+        bool hasLitNsa, hasParallel, hasCar;
+        int numBatteries;
 
-        if (Ccp.CheckAnswer(Answer.ToCharArray()))
-            GetComponent<KMBombModule>().HandlePass();
+        if (serial == null)
+        {
+            // Generate random values for testing in Unity
+            serial = string.Join("", Enumerable.Range(0, 6).Select(i => Rnd.Range(0, 36)).Select(i => i < 10 ? ((char) ('0' + i)).ToString() : ((char) ('A' + i - 10)).ToString()).ToArray());
+            hasLitNsa = Rnd.Range(0, 2) == 0;
+            hasParallel = Rnd.Range(0, 2) == 0;
+            hasCar = Rnd.Range(0, 2) == 0;
+            numBatteries = Rnd.Range(0, 7);
+        }
         else
-            GetComponent<KMBombModule>().HandleStrike();
-
-        Answer = string.Empty;
-    }
-
-    private void SetDisplay()
-    {
-        var indicators = new Dictionary<string, bool>();
-        var indicatorsReponses = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_INDICATOR, null);
-        foreach (var indicatorsReponse in indicatorsReponses)
         {
-            var indicatorDictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(indicatorsReponse);
-            var labelName = indicatorDictionary["label"];
-            var labelStatus = Convert.ToBoolean(indicatorDictionary["on"]);
-            indicators.Add(labelName, labelStatus);
+            hasLitNsa = Bomb.IsIndicatorOn(KMBombInfoExtensions.KnownIndicatorLabel.NSA);
+            hasParallel = Bomb.GetPorts().Contains("Parallel");
+            hasCar = Bomb.IsIndicatorPresent(KMBombInfoExtensions.KnownIndicatorLabel.CAR);
+            numBatteries = Bomb.GetBatteryCount();
         }
 
-        var ports = new List<string>();
-        var portsReponses = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_PORTS, null);
-        foreach (var portsReponse in portsReponses)
+        // Calculate the offset
+        var offset = 0;
+        if (hasLitNsa && hasParallel)
         {
-            var portsDict = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(portsReponse);
-            ports = portsDict["presentPorts"];
+            // Offset stays at 0
+        }
+        else
+        {
+            // Offset by Battery count
+            offset += numBatteries;
+
+            // Offset by Serial vowel check
+            if (serial.Intersect("AEIOU").Any())
+                offset -= 1;
+
+            // Offset by CAR indicator
+            if (hasCar)
+                offset += 1;
+
+            // Offset by Serial even ending
+            if ("02468".Contains(serial.Last()))
+                offset += 1;
         }
 
-        var serial = string.Empty;
-        var serialResponses = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_SERIAL_NUMBER, null);
-        foreach (var serialResponse in serialResponses)
-        {
-            Dictionary<string, string> serialDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(serialResponse);
-            serial = serialDict["serial"];
-        }
-
-        var batteriesCount = 0;
-        var batteriesResponses = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_BATTERIES, null);
-        foreach (var batteriesResponse in batteriesResponses)
-        {
-            Dictionary<string, int> batteriesDict = JsonConvert.DeserializeObject<Dictionary<string, int>>(batteriesResponse);
-            batteriesCount += batteriesDict["numbatteries"];
-        }
-
-        Ccp.CalculateOffset(batteriesCount, serial, ports, indicators);
-        DisplayText.text = Ccp.GetDisplayText();
+        Debug.Log("[CaesarCipher] Offset is " + offset);
+        DisplayText.text = new string(_puzzleWord.Select(ch => (char) ((ch - 'A' - offset + 26) % 26 + 'A')).ToArray());
     }
 }
